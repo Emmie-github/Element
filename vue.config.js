268f715d02dedd81b5bdce9f3ddc1317b38b894b
Emmie-github <aixiao92@foxmail.com>
2019-09-23 20:16:04 +0800

    add component

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  // 方式1
+  // chainWebpack: (config) => {
+  //   config.resolve.alias.set('@element', path.resolve(__dirname, 'src/components/element'));
+  // },
+  // 方式2
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@element': '@/components/element',
+      },
+    },
+  },
+};
